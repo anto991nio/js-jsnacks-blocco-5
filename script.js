baa8af5a9328ee41de5b321f5fcbc942ea7f32f8
anto991nio <antonio.91.cardone@gmail.com>
2021-04-23 15:35:40 +0200

    Termina esercizio

--- script.js
+++ script.js
@@ -60,11 +60,53 @@ var zucchine =[{
 var totalePeso= 0;
 
 
+//Dividi in due array separati le zucchine che misurano meno o più di 15cm.Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
 for( var i=0 ; i<zucchine.length; i++){
     var elementoPeso = zucchine[i].peso
 
     totalePeso+=elementoPeso;
 }
 
-console.log("Le zucchine pesano complessivamente",totalePeso)
+console.log("Le zucchine pesano complessivamente",totalePeso);
+
+
+var zucchineGrandi = [];
+var zucchinePiccole = [];
+
+for( var i=0 ; i<zucchine.length; i++){
+    var elementoLunghezza = zucchine[i].lunghezza
+    if(elementoLunghezza > 15){
+        zucchineGrandi.push(zucchine[i]);
+    }else{
+        zucchinePiccole.push(zucchine[i])
+    }
+
+}
+
+console.log("Queste sono le zucchine grandi :",zucchineGrandi);
+console.log("Queste sono le zucchine piccole :" , zucchinePiccole);
+
+var tototalePesoZucchineGrandi=0;
+for( var i=0 ; i<zucchineGrandi.length; i++){
+    var zucchineGrandiPeso = zucchineGrandi[i].peso
+
+    tototalePesoZucchineGrandi+=zucchineGrandiPeso;
+}
+
+var tototalePesoZucchinePiccole=0;
+
+for( var i=0 ; i<zucchinePiccole.length; i++){
+    var zucchinePiccolePeso = zucchinePiccole[i].peso
+
+    tototalePesoZucchinePiccole+=zucchinePiccolePeso;
+}
+
+
+console.log("Le zucchine grandi pesano :" + " " ,tototalePesoZucchineGrandi, "kg");
+console.log("Le zucchine piccole pesano :" + " " ,tototalePesoZucchinePiccole,"kg");
+
+
+
+
 
